93739dd5fbd860cf78a4d8ad1a861c7b8fcede26
a5535772 <214529283@qq.com>
2019-01-28 18:35:45 +0800

    add Google QPS Lab Codes

---
+++ 01.Google QPS/google-qps/src/main/java/com/leo/labs/qps/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.leo.labs.qps.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+@RestController
+public class IndexController {
+	public static final String Succ = "succ";
+	public static final String Fail = "fail";
+
+	@GetMapping("hello")
+	public String hello() {
+		return Succ;
+	}
+
+	RateLimiter rateLimiter = RateLimiter.create(1);
+
+	@GetMapping("helloLimited")
+	public String helloLimited() {
+		if (rateLimiter.tryAcquire()) {
+			return Succ;
+		} else {
+			return Fail;
+		}
+	}
+}
---
+++ 01.Google QPS/google-qps/src/test/java/com/leo/labs/qps/GoogleQpsRestTemplateTests.java
@@ -0,0 +1,68 @@
+package com.leo.labs.qps;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.leo.labs.qps.controller.IndexController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GoogleQpsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GoogleQpsRestTemplateTests {
+	/**
+	 * @LocalServerPort 提供了 @Value("${local.server.port}") 的代替
+	 */
+	@LocalServerPort
+	private int port;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Before
+	public void setUp() throws Exception {
+		String url = String.format("http://localhost:%d/", port);
+		System.out.println(String.format("port is : [%d]", port));
+		this.base = new URL(url);
+	}
+
+	@Test
+	public void getHelloTest() throws Exception {
+		ResponseEntity<String> response = this.restTemplate.getForEntity(this.base.toString() + "/hello", String.class,
+				"");
+		String respString = response.getBody();
+		assertThat(respString).isEqualTo(IndexController.Succ);
+	}
+	
+	@Test
+	public void getHelloLimitedTest() throws Exception {
+//		String respBody1 = getHelloLimited();
+//		String respBody2 = getHelloLimited();
+//		String respBody3 = getHelloLimited();
+//		assertThat(respBody1).isEqualTo(IndexController.Succ);
+//		assertThat(respBody2).isEqualTo(IndexController.Succ);
+//		assertThat(respBody3).isEqualTo(IndexController.Fail);
+		for (int i = 0; i <10; i++) {
+			System.out.println(getHelloLimited());
+		}
+	}
+
+	private String getHelloLimited() {
+		String respBody=null;
+			ResponseEntity<String> resp = this.restTemplate.getForEntity(this.base.toString() + "/helloLimited", String.class,
+					"");
+			respBody= resp.getBody();
+		return respBody;
+	}	
+}
