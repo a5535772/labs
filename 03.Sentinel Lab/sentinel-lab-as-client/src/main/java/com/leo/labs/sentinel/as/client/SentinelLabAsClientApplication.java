15582607d073fafd085cdcccd84604eb1369b833
a5535772 <214529283@qq.com>
2019-04-23 19:57:38 +0800

    使用 sentinel-dashboard 来操作相关功能

---
+++ 03.Sentinel Lab/sentinel-lab-as-client/src/main/java/com/leo/labs/sentinel/as/client/SentinelLabAsClientApplication.java
@@ -0,0 +1,13 @@
+package com.leo.labs.sentinel.as.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelLabAsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelLabAsClientApplication.class, args);
+	}
+
+}
