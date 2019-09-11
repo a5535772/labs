4fd76c65da52bb4e51a589faf769819a077d389b
leo <214529283@qq.com>
2019-09-11 11:39:34 +0800

    增加了用户的认证信息获取

--- 08.spirng-oauth2/spring-oauth2-04-with-out-hard-code/src/main/java/com/leo/labs/oauth2/browser/Application.java
+++ 08.spirng-oauth2/spring-oauth2-04-with-out-hard-code/src/main/java/com/leo/labs/oauth2/browser/Application.java
@@ -3,7 +3,6 @@ package com.leo.labs.oauth2.browser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,13 +14,5 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	/**
-	 * 127.0.0.1:8080/hello
-	 * <p/>
-	 * 默认用户名 user
-	 */
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello";
-	}
+
 }
