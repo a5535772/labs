a7f8c677bbca16e1eb3e4398aca0f11168f0c60c
leo <214529283@qq.com>
2019-10-12 10:22:29 +0800

    just a test push

---
+++ 08.spirng-oauth2/02.spring-oauth2-social-client/spring-oauth2-social-02-justatest/src/main/java/com/leo/labs/oauth2/authorization/server/TestController.java
@@ -0,0 +1,18 @@
+package com.leo.labs.oauth2.authorization.server;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "1";
+	}
+	
+	@GetMapping("/user/me")
+	public Object getCurrentUser() {
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+}
