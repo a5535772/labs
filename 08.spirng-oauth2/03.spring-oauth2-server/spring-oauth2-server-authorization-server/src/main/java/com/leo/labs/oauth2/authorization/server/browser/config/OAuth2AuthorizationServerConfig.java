ef0994d29e5e763c5d284b64b71f4221a90f256b
leo <214529283@qq.com>
2019-10-14 15:03:53 +0800

    1、使用redis 存储了 token 2、使用mysql存储了授权码

--- 08.spirng-oauth2/03.spring-oauth2-server/spring-oauth2-server-authorization-server/src/main/java/com/leo/labs/oauth2/authorization/server/browser/config/OAuth2AuthorizationServerConfig.java
+++ 08.spirng-oauth2/03.spring-oauth2-server/spring-oauth2-server-authorization-server/src/main/java/com/leo/labs/oauth2/authorization/server/browser/config/OAuth2AuthorizationServerConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -14,25 +16,30 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 	private AuthenticationManager authenticationManager;
 	@Autowired
 	private UserDetailsService simpleInternalMemoryUserDetailsService;
-	
-	  @Override
+	@Autowired
+	private TokenStore redisTokenStore;
+	@Autowired
+	private JdbcAuthorizationCodeServices jdbcAuthorizationCodeServices;
+
+	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-	        clients.inMemory()
-	                .withClient("leo-lab-client-id")
-	                .secret("leo-lab-client-secret")
+		clients.inMemory().withClient("leo-lab-client-id").secret("leo-lab-client-secret")
 //	                .redirectUris("http://localhost:8081/callback") //新增redirect_uri
-	                .authorizedGrantTypes("authorization_code", "client_credentials", "refresh_token",
-	                        "password", "implicit")
-	                .scopes("all")
+				.authorizedGrantTypes("authorization_code", "client_credentials", "refresh_token", "password",
+						"implicit")
+				.scopes("all")
 //	                .resourceIds("oauth2-resource")
 //	                .accessTokenValiditySeconds(120)
 //	                .refreshTokenValiditySeconds(60)
-	                ;
-	    }
+		;
+	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager);
 		endpoints.userDetailsService(simpleInternalMemoryUserDetailsService);// 要使用refresh_token的话，需要额外配置userDetailsService
+		endpoints.tokenStore(redisTokenStore);
+		endpoints.authorizationCodeServices(jdbcAuthorizationCodeServices);
+
 	}
 }
--- 08.spirng-oauth2/03.spring-oauth2-server/spring-oauth2-server-authorization-server/src/main/java/com/leo/labs/oauth2/authorization/server/browser/config/SecuritySupportConfig.java
+++ 08.spirng-oauth2/03.spring-oauth2-server/spring-oauth2-server-authorization-server/src/main/java/com/leo/labs/oauth2/authorization/server/browser/config/SecuritySupportConfig.java
@@ -5,11 +5,15 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import com.leo.labs.oauth2.core.userdetails.SimpleInternalMemoryUserDetailsService;
 
@@ -17,10 +21,22 @@ import com.leo.labs.oauth2.core.userdetails.SimpleInternalMemoryUserDetailsServi
 public class SecuritySupportConfig {
 	@Autowired
 	private DataSource dataSource;
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
 
-	@Bean // 声明 ClientDetails实现
+	@Bean // 使用JdbcClientDetailsService存储租户信息
 	public ClientDetailsService clientDetails() {
 		return new JdbcClientDetailsService(dataSource);
+	}
+
+	@Bean // 声明TokenStore实现，使用redis存储授权的token
+	public TokenStore redisTokenStore() {
+		return new RedisTokenStore(redisConnectionFactory);
+	}
+
+	@Bean // 声明 JdbcAuthorizationCodeServices，使用数据库存储授权码
+	public JdbcAuthorizationCodeServices jdbcAuthorizationCodeServices() {
+		return new JdbcAuthorizationCodeServices(dataSource);
 	}
 
 	@Bean
