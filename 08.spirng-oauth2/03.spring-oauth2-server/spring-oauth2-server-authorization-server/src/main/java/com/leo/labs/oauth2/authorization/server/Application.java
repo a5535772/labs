84b947238369630e2556b8ba23512a20f1d06270
leo <214529283@qq.com>
2019-10-15 09:53:26 +0800

    #引入Spring Session 由于http://127.0.0.1:8081/oauth/authorize?user_oauth_approval=true&scope.all=true&authorize=Authorize 该请求需要用到session，为了保障分布式，引入redis session

--- 08.spirng-oauth2/03.spring-oauth2-server/spring-oauth2-server-authorization-server/src/main/java/com/leo/labs/oauth2/authorization/server/Application.java
+++ 08.spirng-oauth2/03.spring-oauth2-server/spring-oauth2-server-authorization-server/src/main/java/com/leo/labs/oauth2/authorization/server/Application.java
@@ -3,8 +3,10 @@ package com.leo.labs.oauth2.authorization.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 @EnableAuthorizationServer
 public class Application {
 
